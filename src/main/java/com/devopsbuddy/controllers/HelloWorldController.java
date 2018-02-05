10a20eb577874ea5a5d5ecf99ca45b5c704b50b8
SL <shengboliu@yaoo.com>
2018-02-05 12:17:51 -0600

    Enabled Spring security for the website

--- src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
+++ src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
@@ -1,6 +1,6 @@
 package com.devopsbuddy.controllers;
 
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //@Controller
